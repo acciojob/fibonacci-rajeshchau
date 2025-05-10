03d8599fbad6b937717abed62952f8cfd5376d21
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-10 19:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let newarray = [0,1]
+	let first = 1
+
+	while(true){
+		if (arr[newarray[newarray.length-1] === num]) {
+			return first
+		}
+		first += newarray[newarray.length-1];
+		
+	}
 }
 
 module.exports = fibonacci;
